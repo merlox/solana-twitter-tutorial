c1788d10ac61807f308ccae7cab12713defda7fc
merlox <merunasgrincalaitis@gmail.com>
2022-02-12 20:11:04 -0400

    Implemented write tweet with errors

--- programs/tweet/src/lib.rs
+++ programs/tweet/src/lib.rs
@@ -5,10 +5,31 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod tweet {
     use super::*;
-    pub fn initialize(ctx: Context<TweetController>) -> ProgramResult {
+    pub fn setup_platform(ctx: Context<TweetController>) -> ProgramResult {
         let tweet = &mut ctx.accounts.tweet;
         tweet.likes = 0;
         tweet.message = ("").to_string();
+        Ok(())
+    }
+
+    pub fn write_my_tweet(
+        ctx: Context<WriteTweet>,
+        message: String,
+        sender: Pubkey
+    ) -> ProgramResult {
+        let tweet = &mut ctx.accounts.tweet;
+
+        if !tweet.message.trim().is_empty() {
+            return Err(Errors::CantUpdateTweet.into());
+        }
+        if message.trim().is_empty() {
+            return Err(Errors::NoMessage.into());
+        }
+
+        tweet.message = message;
+        tweet.likes = 0;
+        tweet.creator = sender;
+
         Ok(())
     }
 }
@@ -23,11 +44,23 @@ pub struct TweetController<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct WriteTweet<'info> {
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
+}
 
 #[account]
 #[derive(Default)]
 pub struct Tweet {
     message: String,
     likes: u128,
+    creator: Pubkey,
+}
+
+#[error]
+pub enum Errors {
+    #[msg("That tweet message can't be updated")]
+    CantUpdateTweet,
+    #[msg("The tweet message can't be empty")]
+    NoMessage,
 }
