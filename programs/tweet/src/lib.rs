d246ba6ecfefdf039adc37c062cde7d6eaf54542
merlox <merunasgrincalaitis@gmail.com>
2022-02-15 17:19:51 -0400

    deployed program

--- programs/tweet/src/lib.rs
+++ programs/tweet/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2rC3dHHk5B91q27fDqCKEZ9qKT8Cgeo1JpQdsdCeNHs5");
 
 #[program]
 pub mod tweet {
